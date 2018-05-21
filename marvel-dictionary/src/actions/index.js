95127f6d2de4e7add5e07027e12c352044536a40
kenduigraha <ken.duigraha@kudo.co.id>
2018-05-21 14:53:04 +0700

    Add action, http request to marvel get characters end point.

---
+++ marvel-dictionary/src/actions/index.js
@@ -0,0 +1,66 @@
+import * as types from '../constants';
+
+const marvelBaseEndPoint = process.env.REACT_APP_API_MARVEL_BASE_END_POINT;
+const marvelAPIVersion = process.env.REACT_APP_API_MARVEL_VERSION;
+const marvelEndPointGetCharacters = `${marvelBaseEndPoint}${marvelAPIVersion}/public/characters`;
+const marvelPublicKey = process.env.REACT_APP_API_MARVEL_PUBLIC_KEY;
+const marvelPrivateKey = process.env.REACT_APP_API_MARVEL_PRIVATE_KEY;
+
+const date =  new Date();
+const timeStamp = date.getTime();
+
+const paramsValueGetChars = {
+    apikey: marvelPublicKey,
+    ts: timeStamp,
+    hash: md5(timeStamp + marvelPrivateKey + marvelPublicKey),
+};
+
+let loadStateMarvelChars = () => {
+    return {
+        type: types.LOAD_CHARACTERS_MARVEL,
+    }
+}
+
+let loadMarvelCharsSuccess = (dataChars) => {
+    return {
+        type: types.LOAD_CHARACTERS_MARVEL_SUCCESS,
+        payload: dataChars,
+    }
+}
+
+let loadMarvelCharsFailure = () => {
+    return {
+        type: types.LOAD_CHARACTERS_MARVEL_FAILURE,
+    }
+}
+
+
+let loadMarvelChars = () => {
+    marvelEndPointGetCharacters += `?apikey=${marvelPublicKey}&ts=${timeStamp}&hash=${md5(timeStamp + marvelPrivateKey + marvelPublicKey)}`
+
+    return dispatch => {
+        dispatch(loadStateMarvelChars());
+        return fetch(marvelEndPointGetCharacters)
+                .then(results => {
+                    console.log(results);
+                    if (results.status !== 200 ) {
+                    console.log('Upsss');
+                    throw Error(`HTTP Status Code is  ${results.status}`);
+                    }
+                    return results.json()
+                })
+                .then(data => {
+                    if (data.code !== 200) throw Error(`Response Code Data is not 200 !`);
+                    const dataChars = data.data.results;
+                    console.log(dataChars);
+                    dispatch(loadMarvelCharsSuccess(data));
+                })
+                .catch(err => {
+                    console.error(`Ups! Something Happended!`);
+                    console.error(err);
+                    dispatch(loadMarvelCharsFailure());
+                });
+    }
+}
+
+export { loadMarvelChars };
