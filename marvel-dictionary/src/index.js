6a15e5ef10c1a3bc423520076b2727d312ffaa9f
kenduigraha <ken.duigraha@kudo.co.id>
2018-05-21 16:08:40 +0700

    Remove unuse code in index.js file.

--- marvel-dictionary/src/index.js
+++ marvel-dictionary/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Loadable from 'react-loadable';
 import './index.css';
-// import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-// import routes from './routes'
 
 const store = configureStore();
 
