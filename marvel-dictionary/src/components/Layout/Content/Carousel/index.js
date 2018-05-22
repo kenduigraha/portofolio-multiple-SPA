255ceb6bd4d8111a8f933c01d2cd2488eaacaeeb
kenduigraha <ken.duigraha@kudo.co.id>
2018-05-23 05:16:19 +0700

    Update div img to looping.

--- marvel-dictionary/src/components/Layout/Content/Carousel/index.js
+++ marvel-dictionary/src/components/Layout/Content/Carousel/index.js
@@ -2,44 +2,22 @@ import React, { Component } from 'react';
 import { Carousel, Input } from 'antd';
 const Search = Input.Search;
 
+const dataImg = [1, 2, 3, 4, 5];
+
 const CarouselComponent = (props) => {
     return (
         <Carousel>
-            <div>
-                <img
-                    src="/images/carousel-1.jpg"
-                    style={{ width: '100%', margin: 'auto', height: 'auto' }}
-                    alt="marvel #1"
-                />
-            </div>
-            <div>
-                <img
-                    src="/images/carousel-2.jpg"
-                    style={{ width: '100%', margin: 'auto', height: 'auto' }}
-                    alt="marvel #2"
-                />
-            </div>
-            <div>
-                <img
-                    src="/images/carousel-3.jpg"
-                    style={{ width: '100%', margin: 'auto', height: 'auto' }}
-                    alt="marvel #3"
-                />
-            </div>
-            <div>
-                <img
-                    src="/images/carousel-4.jpg"
-                    style={{ width: '100%', margin: 'auto', height: 'auto' }}
-                    alt="marvel #4"
-                />
-            </div>
-            <div>
-                <img
-                    src="/images/carousel-5.jpg"
-                    style={{ width: '100%', margin: 'auto', height: 'auto' }}
-                    alt="marvel #5"
-                />
-            </div>
+            {
+                dataImg.map(no => (
+                    <div>
+                        <img
+                            src={`/images/carousel-${no}.jpg`}
+                            style={{ width: '100%', margin: 'auto', height: 'auto' }}
+                            alt={`marvel carousel #${no}`}
+                        />
+                    </div>
+                ))
+            }
         </Carousel>
     )
 }
