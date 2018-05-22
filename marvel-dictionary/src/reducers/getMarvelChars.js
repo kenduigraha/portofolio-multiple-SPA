7ea4b7e4ece3afae35cdb6bdce51165e28c3632f
kenduigraha <ken.duigraha@kudo.co.id>
2018-05-22 23:26:41 +0700

    Add reducer for update infinity list handle.

--- marvel-dictionary/src/reducers/getMarvelChars.js
+++ marvel-dictionary/src/reducers/getMarvelChars.js
@@ -1,23 +1,33 @@
 import {
     LOAD_CHARACTERS_MARVEL,
     LOAD_CHARACTERS_MARVEL_SUCCESS,
-    LOAD_CHARACTERS_MARVEL_FAILURE
+    LOAD_CHARACTERS_MARVEL_FAILURE,
+    UPDATE_FLAG_INFINITY_CHARACTERS_MARVEL
 } from '../constants';
 
 const initialState = {
     loading: true,
+    infinity: false,
 };
 
 export default (state = initialState, action) => {
     switch(action.type) {
         case LOAD_CHARACTERS_MARVEL:
-            return state;
+            return Object.assign({}, state, {
+                loading: true
+            });
         
         case LOAD_CHARACTERS_MARVEL_SUCCESS:
             return {
                 loading: false,
+                infinity: false,
                 payload: action.payload,
             };
+
+        case UPDATE_FLAG_INFINITY_CHARACTERS_MARVEL:
+            return Object.assign({}, state, {
+                infinity: action.payload.data
+            });
 
         case LOAD_CHARACTERS_MARVEL_FAILURE:
         default:
