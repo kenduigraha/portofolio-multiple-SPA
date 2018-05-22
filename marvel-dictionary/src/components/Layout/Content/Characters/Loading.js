6d6ebdbcd9508b9de4c2ce889b34ae2c59255e85
kenduigraha <ken.duigraha@kudo.co.id>
2018-05-22 23:23:42 +0700

    Add loading component for characters.

---
+++ marvel-dictionary/src/components/Layout/Content/Characters/Loading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+const CharactersLoading = () => {
+    return (
+        <span>
+            <Button shape="circle" loading />
+        </span>
+    )
+}
+
+export default CharactersLoading;
